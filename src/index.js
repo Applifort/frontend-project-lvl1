0ac1df346c026388ff832226af207895c89c8143
GabitovArtur <coolermast@gmail.com>
2019-11-01 18:59:01 +0500

    First stable version

--- src/index.js
+++ src/index.js
@@ -1,8 +1,4 @@
 import readlineSync from 'readline-sync';
 
-console.log('Welcome to the Brain Games!');
-console.log('ddfff');
-
-const actual = readlineSync.question('May I have your name? ');
+const actual = () => readlineSync.question('May I have your name? ');
 export default actual;
-console.log(`Hello, ${actual} !`);
